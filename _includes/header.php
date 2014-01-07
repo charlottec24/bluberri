c7aa763defce7bccea95c4d3257e3bb759624205
Charlotte Cameron <charlottemidgley@gmail.com>
2014-01-07 15:50:21 +0000

    Added media query breakpoints for menu

--- _includes/header.php
+++ _includes/header.php
@@ -2,6 +2,22 @@
 	
 		<!-- BEGIN HEADER -->
 		<header>
-		
+
+			<div class="container-16">
+
+				<div class="grid-16">
+					<nav role="navigation">
+						<ul>
+							<li><a href="">About</a></li>
+							<li><a class="logo" href="">Services</a></li>
+							<li><a class="logo" href=""><img src="/_assets/images/logo.png"></a></li>
+							<li><a href="">Portfolio</a></li>
+							<li><a class="last" href="">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+
+			</div>
+
 		</header>
 		<!-- END HEADER -->

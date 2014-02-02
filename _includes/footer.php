9cd6850eb6b21dcf8c78f6283b730bf03e4f5de5
Charlotte Cameron <charlottemidgley@gmail.com>
2014-02-02 15:34:36 +0000

    Started making footer.

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,5 +1,37 @@
 		<!-- BEGIN FOOTER -->
 		<footer>
+
+			<div class="container-16">
+
+				<div class="grid-5">
+					<h2>Contact</h2>
+					<ul>
+						<li>E: enquiries@bluberri-designs.co.uk</li>
+						<li>T: 07855 084128</li>
+					</ul>
+				</div>
+				<div class="grid-5">
+					<h2>What we do</h2>
+					<ul>
+						<li>Bespoke web design</li>
+						<li>Printed marketing</li>
+						<li>Photography</li>
+						<li>Website development</li>
+					</ul>
+					<ul>
+						<li>Branding</li>
+						<li>Email marketing</li>
+						<li>Content Management systems</li>
+					</ul>
+				</div>
+				<div class="grid-5">
+					<h2><strong>Simple.</strong></h2>
+					<h2><strong>Beautiful.</strong></h2>
+					<h2><strong>Practical</strong> design</h2>
+				</div>
+
+			</div>
+
 			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 			<script src="/_assets/javascript/jquery.slicknav.min.js"></script>
 			<script type="text/javascript">
@@ -10,7 +42,7 @@
 		</footer>
 		<!-- END FOOTER -->
 		
-	</div>
+	
 	
 </body>
 </html>
